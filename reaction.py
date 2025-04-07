b93d95041caf0ccf11c3f2a478ce2de7df76d406
xab901 <xab901@outlook.com>
2025-04-07 03:58:56 +0100

    Add feature: player name display

--- reaction.py
+++ reaction.py
@@ -6,13 +6,19 @@ led = LED(4) # set up the pin as output
 right_button = Button(15)
 left_button = Button(14)
 
+left_name = input('left player name is: ')
+right_name = input('right player name is: ')
+
 led.on()
 sleep(uniform(5,10)) # randomize sleep time
 led.off()
 
 def pressed(button):
 	# show which pin button was on
-	print(str(button.pin.number) + 'won the game')
+	if button.pin.number == 14:
+		print(left_name + 'won the game')
+	else:
+		print(right_name + 'won the game')
 
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
