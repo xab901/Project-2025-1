4e20f65bd27f42a20ddc8554a0029f7ce286efb6
xab901 <xab901@outlook.com>
2025-04-07 03:54:34 +0100

    Add feature: button detection

--- reaction.py
+++ reaction.py
@@ -3,6 +3,16 @@ from time import sleep
 from random import uniform
 
 led = LED(4) # set up the pin as output
+right_button = Button(15)
+left_button = Button(14)
+
 led.on()
 sleep(uniform(5,10)) # randomize sleep time
 led.off()
+
+def pressed(button):
+	# show which pin button was on
+	print(str(button.pin.number) + 'won the game')
+
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
