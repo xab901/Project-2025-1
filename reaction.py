68f12e911c673b494cc45ac1b0fa9006bcf730d7
xab901 <xab901@outlook.com>
2025-04-11 12:13:58 +0100

    Add: game loop

--- reaction.py
+++ reaction.py
@@ -10,19 +10,25 @@ left_button = Button(14)
 left_name = input('left player name is: ')
 right_name = input('right player name is: ')
 
-led.on()
-sleep(uniform(5,10)) # randomize sleep time
-led.off()
+def start_round():
+	led.on()
+	sleep(uniform(5, 10))
+	led.off()
 
 def pressed(button):
 	# show which pin button was on
 	if button.pin.number == 14:
-		print(left_name + 'won the game')
+		print(left_name + ' won the game')
 	else:
-		print(right_name + 'won the game')
-	exit()
+		print(right_name + ' won the game')
+	print("Let's play another round......")
+	sleep(1)
+	start_round()
 
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
+
+# Start the first round
+start_round()
 
 pause()
